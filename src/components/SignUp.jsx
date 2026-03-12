41d31626d18565fd5c5c8c7f6d278b6b04a48e46
Frankrust <boomboxdaniel17@gmail.com>
2026-03-12 20:43:13 +0100

    feat(auth) initial design of the signup page (redesign later)

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -8,7 +8,7 @@ import {
   GoogleAuthProvider,
   updateProfile,
 } from "firebase/auth";
-import { signUpSchema } from "./functions/validationSchema";
+import { signUpSchema } from "../functions/validationSchema.js";
 
 export default function SignUp() {
   const googleProvider = new GoogleAuthProvider();
@@ -68,147 +68,179 @@ export default function SignUp() {
 
   return (
     <section className="min-h-screen flex justify-center">
-      <div className="flex flex-row w-full max-w-6xl p-8">
-        <div className="hidden lg:flex flex-1"></div>
-        <div className="flex-1 flex flex-col justify-center">
-          <p className="text-5xl mb-5 font-bold">
-            Get a cup of coffee for us, create your account
-          </p>
+      <div className="flex flex-row w-full max-w-7xl p-8">
+        <div className="hidden lg:flex flex-7">
+          <img
+            src="/images/login.svg"
+            alt="login"
+            className="h-115 fixed mt-8"
+          />
+        </div>
+        <div className="flex-6">
+          <div className="flex flex-col mx-auto w-100 rounded-3xl bg-white p-7 mt-9">
+            <div className="text-center">
+              <p className="text-2xl font-bold">Create your account</p>
+              <p className="mb-5 text-gray-600">
+                Hey there, create a café one account to complete your order.
+                It's quick and easy!
+              </p>
+            </div>
 
-          <form
-            className="flex w-full max-w-md flex-col"
-            onSubmit={formik.handleSubmit}
-          >
-            {/* NAME INPUT */}
-            <label
-              className={`input validator mb-1 w-full ${
-                formik.touched.name && formik.errors.name
-                  ? "border-red-500"
-                  : ""
-              }`}
+            <form
+              className="flex w-full max-w-md flex-col"
+              onSubmit={formik.handleSubmit}
             >
-              <i className="bx bx-user opacity-50"></i>
-              <input
-                type="text"
-                placeholder="Username"
-                {...formik.getFieldProps("name")}
-              />
-            </label>
-            {formik.touched.name && formik.errors.name && (
-              <div className="text-red-500 text-xs mb-3">
-                {formik.errors.name}
-              </div>
-            )}
-
-            {/* EMAIL INPUT */}
-            <label
-              className={`input validator mb-1 w-full ${
-                formik.touched.email && formik.errors.email
-                  ? "border-red-500"
-                  : ""
-              }`}
-            >
-              <i className="bx bx-envelope opacity-50"></i>
-              <input
-                type="email"
-                placeholder="mail@site.com"
-                {...formik.getFieldProps("email")}
-              />
-            </label>
-            {formik.touched.email && formik.errors.email && (
-              <div className="text-red-500 text-xs mb-3">
-                {formik.errors.email}
-              </div>
-            )}
-
-            {/* PASSWORD INPUT */}
-            <label
-              className={`input validator mb-1 w-full relative ${
-                formik.touched.password && formik.errors.password
-                  ? "border-red-500"
-                  : ""
-              }`}
-            >
-              <i className="bx bx-lock-alt opacity-50"></i>
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Password"
-                {...formik.getFieldProps("password")}
-              />
-              <button
-                type="button"
-                className="absolute right-3 top-1/2 -translate-y-1/2"
-                onClick={() => setShowPassword(!showPassword)}
+              {/* NAME INPUT */}
+              <label
+                className={`input validator mb-3 w-full ${
+                  formik.touched.name && formik.errors.name
+                    ? "border-red-500"
+                    : ""
+                }`}
               >
-                <i
-                  className={`bx ${
-                    showPassword ? "bx-show" : "bx-hide"
-                  } opacity-50 text-xl`}
-                ></i>
-              </button>
-            </label>
-            {formik.touched.password && formik.errors.password && (
-              <div className="text-red-500 text-xs mb-3">
-                {formik.errors.password}
-              </div>
-            )}
-
-            {/* CONFIRM PASSWORD INPUT */}
-            <label
-              className={`input validator mb-1 w-full relative ${
-                formik.touched.confirmPassword && formik.errors.confirmPassword
-                  ? "border-red-500"
-                  : ""
-              }`}
-            >
-              <i className="bx bx-lock-alt opacity-50"></i>
-              <input
-                type={showConfirmPassword ? "text" : "password"}
-                placeholder="Confirm Password"
-                {...formik.getFieldProps("confirmPassword")}
-              />
-              <button
-                type="button"
-                className="absolute right-3 top-1/2 -translate-y-1/2"
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-              >
-                <i
-                  className={`bx ${
-                    showConfirmPassword ? "bx-show" : "bx-hide"
-                  } opacity-50 text-xl`}
-                ></i>
-              </button>
-            </label>
-            {formik.touched.confirmPassword &&
-              formik.errors.confirmPassword && (
-                <div className="text-red-500 text-xs mb-3">
-                  {formik.errors.confirmPassword}
+                <i className="bx bx-user opacity-50"></i>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  {...formik.getFieldProps("name")}
+                />
+              </label>
+              {formik.touched.name && formik.errors.name && (
+                <div className="text-red-500 text-xs mb-2">
+                  {formik.errors.name}
                 </div>
               )}
 
-            {formik.status && (
-              <div className="text-red-600 font-bold mb-3">{formik.status}</div>
-            )}
+              {/* EMAIL INPUT */}
+              <label
+                className={`input validator mb-3 w-full ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500"
+                    : ""
+                }`}
+              >
+                <i className="bx bx-envelope opacity-50"></i>
+                <input
+                  type="email"
+                  placeholder="mail@site.com"
+                  {...formik.getFieldProps("email")}
+                />
+              </label>
+              {formik.touched.email && formik.errors.email && (
+                <div className="text-red-500 text-xs mb-2">
+                  {formik.errors.email}
+                </div>
+              )}
 
-            <button
-              type="submit"
-              className="btn btn-primary mt-4"
-              disabled={formik.isSubmitting}
-            >
-              {formik.isSubmitting ? "Processing..." : "Sign Up"}
-            </button>
+              {/* PASSWORD INPUT */}
+              <label
+                className={`input validator mb-3 w-full relative ${
+                  formik.touched.password && formik.errors.password
+                    ? "border-red-500"
+                    : ""
+                }`}
+              >
+                <i className="bx bx-lock-alt opacity-50"></i>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  {...formik.getFieldProps("password")}
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-1/2 -translate-y-1/2"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  <i
+                    className={`bx ${
+                      showPassword ? "bx-show" : "bx-hide"
+                    } opacity-50 text-xl`}
+                  ></i>
+                </button>
+              </label>
+              {formik.touched.password && formik.errors.password && (
+                <div className="text-red-500 text-xs mb-2">
+                  {formik.errors.password}
+                </div>
+              )}
 
-            <div className="divider">OR</div>
+              {/* CONFIRM PASSWORD INPUT */}
+              <label
+                className={`input validator mb-3 w-full relative ${
+                  formik.touched.confirmPassword &&
+                  formik.errors.confirmPassword
+                    ? "border-red-500"
+                    : ""
+                }`}
+              >
+                <i className="bx bx-lock-alt opacity-50"></i>
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  {...formik.getFieldProps("confirmPassword")}
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-1/2 -translate-y-1/2"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  <i
+                    className={`bx ${
+                      showConfirmPassword ? "bx-show" : "bx-hide"
+                    } opacity-50 text-xl`}
+                  ></i>
+                </button>
+              </label>
+              {formik.touched.confirmPassword &&
+                formik.errors.confirmPassword && (
+                  <div className="text-red-500 text-xs mb-2">
+                    {formik.errors.confirmPassword}
+                  </div>
+                )}
 
-            <button
-              type="button"
-              className="btn btn-outline"
-              onClick={handleGoogleAuth}
-            >
-              <i className="bx bxl-google mr-2"></i>
-              Continue with Google
-            </button>
-          </form>
+              {formik.status && (
+                <div className="text-red-600 font-bold mb-3">
+                  {formik.status}
+                </div>
+              )}
+
+              <button
+                type="submit"
+                className="btn btn-primary mt-4"
+                disabled={formik.isSubmitting}
+              >
+                {formik.isSubmitting ? "Processing..." : "Sign Up"}
+              </button>
+
+              <div className="divider text-gray-600">or</div>
+
+              <button
+                type="button"
+                className="btn btn-outline"
+                onClick={handleGoogleAuth}
+              >
+                <img src="images/google-icon.svg" alt="" className="w-4" />
+                Continue with Google
+              </button>
+            </form>
+            <p className="text-sm text-center text-gray-600 mt-5">
+              By continuing, you agree to our{" "}
+              <a
+                href=""
+                className="text-primary underline text-md font-semibold"
+              >
+                Terms of Use{" "}
+              </a>
+              and{" "}
+              <a
+                href=""
+                className="text-primary underline text-md font-semibold"
+              >
+                Privacy Policy
+              </a>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </section>
